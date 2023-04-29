7bf3fc5a0d8914cdd4642ff0d46fbcd93f00b546
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-04-29 14:32:27 +0300

    Add cmake generate protos from proto submodule

---
+++ grpc-server/cmake/proto.cmake
@@ -0,0 +1,36 @@
+find_package(Protobuf REQUIRED)
+find_program(_GRPC_CPP_PLUGIN_LOCATION grpc_cpp_plugin)
+
+
+set(PROTOC_OUT_DIR ${CMAKE_CURRENT_BINARY_DIR}/cws_proto)
+file(MAKE_DIRECTORY ${PROTOC_OUT_DIR})
+
+
+set(PROTO_FILES
+  ${PROTO_LOC}/cws/hello.proto
+)
+
+
+add_library(cws-proto ${PROTO_FILES})
+
+target_include_directories(cws-proto PUBLIC
+  ${PROTOC_OUT_DIR}
+)
+
+protobuf_generate(
+  TARGET cws-proto
+  LANGUAGE cpp
+  IMPORT_DIRS ${Protobuf_INCLUDE_DIR}
+  PROTOC_OUT_DIR ${PROTOC_OUT_DIR}
+  APPEND_PATH ${PROTO_LOC}
+)
+
+protobuf_generate(
+  TARGET cws-proto
+  LANGUAGE grpc
+  GENERATE_EXTENSIONS .grpc.pb.h .grpc.pb.cc
+  PLUGIN "protoc-gen-grpc=${_GRPC_CPP_PLUGIN_LOCATION}"
+  IMPORT_DIRS ${Protobuf_INCLUDE_DIR}
+  PROTOC_OUT_DIR ${PROTOC_OUT_DIR}
+  APPEND_PATH ${PROTO_LOC}
+)
---
+++ grpc-server/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include <hello.pb.h>
+
+int main() {
+  std::cout << "Server" << std::endl;
+}
